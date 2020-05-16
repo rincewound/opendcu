1b552265e62177de0cf229d9c16ce952247304df
thefux <abdou.rakez@gmail.com>
2020-05-16 11:24:46 +0200

    uncomment code

--- src/ui/preload.js
+++ src/ui/preload.js
@@ -2,13 +2,7 @@ let {
 	webFrame
 } = require("electron");
 
-process.once("loaded", () => {
-	// Allow window.fetch() to access app files
-	// webFrame.registerURLSchemeAsPrivileged("app", {
-	// 	secure: true,
-	// 	bypassCSP: false,
-	// 	allowServiceWorkers: true,
-	// 	supportFetchAPI: true,
-	// 	corsEnabled: false
-	// });
+var _process = process;
+process.once('loaded', function() {
+  global.process= _process;
 });
