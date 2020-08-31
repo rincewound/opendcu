18dcf5a4e0e98d29b31075082871181b095a185c
rincewound <aaaargh@minddebugger.com>
2020-08-31 16:21:42 +0200

    Add framecontact, add switching of door open profile

--- barracuda_modules/adcm/src/components/mod.rs
+++ barracuda_modules/adcm/src/components/mod.rs
@@ -9,8 +9,10 @@ use barracuda_core::profile::*;
 mod electricstrike;
 mod accessgranted;
 mod outputcomponentbase;
+mod framecontact;
 
-enum DoorEvent
+#[derive(Copy, Clone)]
+pub enum DoorEvent
 {
     Opened,
     Closed,
@@ -23,27 +25,33 @@ enum DoorEvent
     Block
 }
 
-pub trait InputComponent
+pub trait InputComponent: Send
 {
     fn on_input_change(&mut self, event: &InputEvent);
     fn on_door_event(&mut self, event: DoorEvent);
 }
 
-pub trait OutputComponent
+pub trait OutputComponent: Send
 {
     fn on_profile_change(&mut self, event: &ProfileChangeEvent);
     fn on_door_event(&mut self, event: DoorEvent);
 }
 
+pub trait VirtualComponent: Send
+{
+    fn on_door_event(&mut self, event: DoorEvent);
+}
 
-struct Passageway
+
+pub struct Passageway
 {
     id: u32,
     door_open_profile_id: u32,
     //bin_prof_rx: Arc<GenericReceiver<ProfileChangeEvent>>,  // <-- Central dispatch here?
     //input_rx: Arc<GenericReceiver<InputEvent>>,             // <-- Central dispatch here?
     input_components: Vec<Box<dyn InputComponent>>,
-    output_components: Vec<Box<dyn OutputComponent>>
+    output_components: Vec<Box<dyn OutputComponent>>,
+    virtual_components: Vec<Box<dyn VirtualComponent>>
 }
 
 impl Passageway
@@ -57,6 +65,17 @@ impl Passageway
 
         // ToDo: if the profile is our door open profile, we have
         // to adjust the doorstate here as well
+        if event.profile_id == self.door_open_profile_id
+        {
+            if event.profile_state == ProfileState::Active
+            {
+                self.handle_door_event(DoorEvent::ReleasedPermanently);
+            }
+            if event.profile_state == ProfileState::Inactive
+            {
+                self.handle_door_event(DoorEvent::NormalOperation);
+            }
+        }
     }
 
     pub fn on_input_change(&mut self, event: &InputEvent)
@@ -67,9 +86,31 @@ impl Passageway
         }
     }
 
-    pub fn on_door_open_request(&mut self, request: DoorOpenRequest)
+    pub fn handle_door_event(&mut self, event: DoorEvent)
     {
+        for v in self.output_components.iter_mut()
+        {
+            v.on_door_event(event);
+        }
+
+        for v in self.input_components.iter_mut()
+        {
+            v.on_door_event(event);
+        }
+
+        for v in self.virtual_components.iter_mut()
+        {
+            v.on_door_event(event);
+        }   
+    }
+
+    pub fn on_door_open_request(&mut self, request: &DoorOpenRequest)
+    {
+        // Check if AP belongs to this door
+
         // Check doorstate: If we're blocked, signal this, otherwise
         // signal access granted here and release the door.
+
+        self.handle_door_event(DoorEvent::ReleaseOnce);
     }
 }
--- barracuda_modules/adcm/src/lib.rs
+++ barracuda_modules/adcm/src/lib.rs
@@ -1,14 +1,12 @@
-extern crate barracuda_core;
-
-//extern crate barracuda_hal;
 
 use barracuda_core::core::broadcast_channel::*;
 use barracuda_core::core::channel_manager::*;
-use barracuda_core::core::{shareable::Shareable, bootstage_helper::*, SystemMessage};
+use barracuda_core::core::{shareable::Shareable, bootstage_helper::*, SystemMessage, event::DataEvent};
 use barracuda_core::{Handler, cfg::{ConfigMessage, cfgholder::*, self}};
 use barracuda_core::trace::*;
-use barracuda_core::dcm::DoorOpenRequest;
+use barracuda_core::{io::InputEvent, dcm::DoorOpenRequest, profile::ProfileChangeEvent, select_chan, select_chan_with_timeout, wait_for_with_timeout, wait_for};
 use std::{sync::Arc, thread};
+use components::Passageway;
 
 mod components;
 
@@ -17,19 +15,88 @@ const MODULE_ID: u32 = 0x03000000;
 pub fn launch(chm: &mut ChannelManager)
 {    
     let tracer = trace_helper::TraceHelper::new("DCM/ADCM".to_string(), chm);
-    // let mut adcm = ADCM::new(tracer, chm);
-    // thread::spawn(move || {  
-    //     adcm.init();   
-    //     loop 
-    //     {
-    //         if !adcm.do_request()
-    //         {
-    //             break;
-    //         }
-    //     }   
+    let mut adcm = ADCM::new(tracer, chm);
+    thread::spawn(move || {  
+        adcm.init();   
+        loop 
+        {
+            if !adcm.run()
+            {
+                break;
+            }
+        }   
         
-    // });
+    });
 }
+
+struct ADCM
+{
+    bin_prof_rx: Arc<GenericReceiver<ProfileChangeEvent>>,  
+    input_rx: Arc<GenericReceiver<InputEvent>>, 
+    door_req_rx: Arc<GenericReceiver<DoorOpenRequest>>,
+    passageways: Vec<Passageway>
+}
+
+impl ADCM
+{
+    pub fn new(tracer: trace_helper::TraceHelper, chm: &mut ChannelManager) -> Self
+    {
+        Self
+        {
+            bin_prof_rx: chm.get_receiver(),
+            input_rx: chm.get_receiver(),
+            door_req_rx: chm.get_receiver(),
+            passageways: vec![]
+        }
+    }
+
+    pub fn init(&mut self)
+    {
+        // Load passageways!
+    }
+
+    pub fn run(&mut self) -> bool
+    {        
+        let queue_id = select_chan!(self.bin_prof_rx, self.input_rx, self.door_req_rx);
+        match queue_id
+        {
+            0 => self.do_bin_prof_event(),
+            1 => self.do_input_event(),
+            2 => self.do_door_request(),
+            _ => return false
+        }
+        true
+    }
+
+    fn do_input_event(&mut self)
+    {
+        let input_event = self.input_rx.receive();
+        for passageway in self.passageways.iter_mut()
+        {            
+            passageway.on_input_change(&input_event);
+        }
+    }
+
+    fn do_bin_prof_event(&mut self)
+    {
+        let binprof_event = self.bin_prof_rx.receive();
+        for passageway in self.passageways.iter_mut()
+        {            
+            passageway.on_profile_change(&binprof_event);
+        }
+    }
+
+    fn do_door_request(&mut self)
+    {
+        let door_request = self.door_req_rx.receive();
+        for passageway in self.passageways.iter_mut()
+        {            
+            passageway.on_door_open_request(&door_request);
+        }
+    }
+
+}
+
 
 /*
     Data consumed by this module:
